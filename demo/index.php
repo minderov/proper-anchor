7a454636ae9db5109730984f9877c5115fb6d1fa
Alex Minderov <minderov@ezfive.com>
2015-12-20 20:43:51 -0800

    Initial commit

---
+++ demo/index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="main.css" type="text/css">
+    <title>Proper HTML anchor</title>
+</head>
+
+<body>
+<div class="main">
+
+    <div class="links-block">
+        <h2>Normal anchor links</h2>
+        <ul>
+            <li><a href="#mandatory-but-not-really">Mandatory but not really</a></li>
+            <li><a href="#women-serving-too">Women are serving too!</a></li>
+            <li><a href="#terrifying-numbers">Terrifying number</a> <i>(You can only guess what the author meant)</i></li>
+        </ul>
+    </div>
+
+    <div class="links-block">
+        <h2>Proper anchor links</h2>
+        <ul>
+            <li><a class="anchorLink" href="#mandatory-but-not-really">Mandatory but not really</a></li>
+            <li><a class="anchorLink" href="#women-serving-too">Women are serving too!</a></li>
+            <li><a class="anchorLink" href="#terrifying-numbers">Terrifying numbers</a></li>
+        </ul>
+    </div>
+
+    <h1>Military conscription by country</h1>
+
+    <h2>China</h2>
+    <p>
+        Universal conscription in China dates back to the State of Qin, which eventually became the Qin Empire of 221 BC. Following unification, historical records show that a total of 300,000 conscript soldiers and 500,000 conscript labourers constructed the Great Wall of China.
+    </p>
+    
+    <p>
+        In the following dynasties, universal conscription was abolished and reintroduced on numerous occasions.
+    </p>
+    
+    <p>
+        <a name="mandatory-but-not-really"></a>
+        As of 2011, universal military conscription is theoretically mandatory in the People's Republic of China, and reinforced by law. However, due to the large population of China and large pool of candidates available for recruitment, the People's Liberation Army has always had sufficient volunteers, so conscription has not been required in practice at all
+    </p>
+
+    <h2>Israel</h2>
+
+    <p>
+        In Israel, the Muslim and Christian Arab minority are exempt from mandatory service, as are permanent residents such as the Druze of the Golan Heights. Male Ultra-Orthodox Jews may apply for a deferment of draft to study in Yeshiva, and the deferment tends to become an exemption, while female religious Jews can be exempted after presenting "religious declaration" to the IDF authorities, and some (primarily National Religious or Modern Orthodox) choose to volunteer for national service instead. Male Druze and Circassian Israeli citizens are liable, by agreement with their community leaders (Female Druze and Circassian are exempt from service). Members of the exempted groups can still volunteer, including Bedouin males, who serve in relatively large numbers.
+    </p>
+
+    <h2>The Netherlands</h2>
+
+    <p>
+        Conscription, which was called "Service Duty" (Dutch: <i>dienstplicht</i>) in the Netherlands, was first employed in 1810 by French occupying forces. Napoleon's brother Louis Bonaparte, who was King of Holland from 1806 to 1810, had tried to introduce conscription a few years earlier, unsuccessfully. Every man aged 20 years or older had to enlist. By means of drawing lots it was decided who had to undertake service in the French army. It was possible to arrange a substitute against payment.
+    </p>
+    
+    <p>
+        Later on, conscription was used for all men over the age of 18. Postponement was possible, due to study, for example. Conscientious objectors could perform an alternative civilian service instead of military service. For various reasons, this forced military service was criticized at the end of the twentieth century. Since the Cold War was over, so was the direct threat of a war. Instead, the Dutch army was employed in more and more peacekeeping operations. The complexity and danger of these missions made the use of conscripts controversial. Furthermore, the conscription system was thought to be unfair as only men were drafted.
+    </p>
+    
+    <p>
+        In the European part of Netherlands, compulsory attendance has been officially suspended since 1 May 1997. Between 1991 and 1996, the Dutch armed forces phased out their conscript personnel and converted to an all-volunteer force. The last conscript troops were inducted in 1995, and demobilized in 1996. The suspension means that citizens are no longer forced to serve in the armed forces, as long as it is not required for the safety of the country. Since then, the Dutch army is an all-volunteer force. However, to this day, every male citizen aged 17 gets a letter in which he is told that he has been registered but does not have to present himself for service. The Dutch army allowed its male soldiers to have long hair from the early 1970s to the end of conscription in the mid-1990s.
+    </p>
+    
+    <p>
+        Even though it is generally thought that conscription has been abolished in the Netherlands, it is compulsory attendance that was abolished, not conscription. The laws and systems which provide for the conscription of armed forces personnel still remain in place.
+    </p>
+    
+    <h2>Norway</h2>
+    <p>
+        <a name="women-serving-too"></a>
+        Norway employs a weak form of mandatory military service for men and women. In practice recruits are not forced to serve, instead only those who are motivated are selected. About 60,000 Norwegians are available for conscription every year, but only 8,000 to 10,000 are conscripted. On 14 June 2013 the Norwegian Parliament voted to extend conscription to women making Norway the first NATO member and first European country to make national service compulsory for both men and women. There is a right of conscientious objection.
+    </p>
+
+    <h2>United States</h2>
+    <p>
+        In the United States, conscription, also called "the draft", ended in 1973, but males between 18 and 25 are required to register with the Selective Service System to enable a reintroduction of conscription if necessary. President Gerald Ford suspended mandatory draft registration in 1975, but President Jimmy Carter reinstated that requirement when the Soviet Union intervened in Afghanistan. Selective Service registration is still required of all young men, although the draft has not been used since 1973.
+    </p>
+
+    <h2>United Kingdom</h2>
+    <p>
+        The United Kingdom introduced conscription to full-time military service for the first time in January 1916 (the eighteenth month of World War I) and abolished it in 1920. Ireland, then part of the United Kingdom, was excepted from the original 1916 military service legislation, and although further legislation in 1918 gave power for an extension of conscription to Ireland, the power was never put into effect.
+    </p>
+    
+    <p>
+        Conscription was reintroduced in 1939, in the lead up to World War II, and continued in force until 1963. Northern Ireland was excepted from conscription legislation throughout the whole period.
+    </p>
+    
+    <p>
+        <a name="terrifying-numbers"></a>
+        In all, 8,000,000 men were conscripted in the Second World War, as well as several hundred thousand younger single women. The introduction of conscription in May 1939, before the war began, was partly due to pressure from the French, who emphasized the need for a large British army to oppose the Germans. From early 1942 unmarried women age 19–30 were conscripted. Most were sent to the factories, but they could volunteer for the Auxiliary Territorial Service (ATS) and other women's services. None was assigned to combat roles unless she volunteered. By 1943 women were liable to some form of directed labour up to age 51. During the Second World War, 1.4 million British men volunteered for service and 3.2 million were conscripted. Conscripts comprised 50% of the Royal Air Force, 60% of the Royal Navy and 80% of the British Army.
+    </p>
+    
+    <p>
+        The abolition of conscription in Britain was announced on 4 April 1957, by new prime minister Harold Macmillan, with the last conscripts being recruited three years later.
+    </p>
+</div>
+
+
+<script type="text/javascript" src="jquery.js"></script>
+<script type="text/javascript" src="proper-anchor.js"></script>
+<link href="proper-anchor.css" rel="stylesheet"/>
+<script>
+    $(document).ready(function() {
+        $('.anchorLink').properAnchor();
+    });
+</script>
+</body>
+</html>
