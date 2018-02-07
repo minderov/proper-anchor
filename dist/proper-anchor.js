38f332b9000fff1657a582ec2b98ef5faf31fc2a
Ryan Peterson <PetersonRyan97@gmail.com>
2018-02-06 16:53:04 -0800

    id fallback and more options
    If the anchor href is not the name of an element, it will look for the id instead.
    
    highlight option (boolean) controls whether the anchor will be highlighted after arriving.
    
    scrollToCenter option (boolean) will control whether the anchor scrolls to the top of the page or the center.

--- dist/proper-anchor.js
+++ dist/proper-anchor.js
@@ -4,7 +4,9 @@
 
         var settings = $.extend({
             changeUrl: false,
-            scrollDuration: 400
+            scrollDuration: 400,
+            highlight: true,
+            scrollToCenter: true
         }, options );
 
 
@@ -14,13 +16,16 @@
 
             var hashName = $.attr(this, 'href').substr(1);
             var anchor = $('[name="' + hashName + '"]');
+            if (!anchor.length) anchor = $('#' + hashName); //Fallback to id if name is not available
             var anchorParent = anchor.parent();
             var scrollMultiplier = 0.3, windowHeight = $(window).height(), anchorParentHeight = anchorParent.height();
-            anchorParent.addClass('anchorBox');
-            anchorParent.addClass('colored');
+            if (settings.highlight){
+                anchorParent.addClass('anchorBox');
+                anchorParent.addClass('colored');
+            }
 
-            // Calculting coordinates to scroll to, in which the target element will be just a bit higher than the middle of browser's window
-            if (anchorParentHeight >= windowHeight) {
+            // Calculating coordinates to scroll to, in which the target element will be just a bit higher than the middle of browser's window
+            if (anchorParentHeight >= windowHeight || !settings.scrollToCenter) {
                 var scrollTo = anchor.offset().top;
             } else {
                 var scrollTo = anchor.offset().top - windowHeight/2 + anchorParentHeight/2 + (windowHeight/2 - anchorParentHeight/2)*scrollMultiplier ;
